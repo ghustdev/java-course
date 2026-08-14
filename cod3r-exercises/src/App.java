8e26ac280d8882df9d418fea7ad9155ecc5af16f
ghustdev <cardosogustavo@discente.ufg.br>
2026-08-14 16:35:48 -0300

    feat: funtamentos de Java

--- cod3r-exercises/src/App.java
+++ cod3r-exercises/src/App.java
@@ -1,6 +1,8 @@
 import initial.code.Artigo;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Date;
 
 public class App {
 	public static void main(String[] args) throws Exception {
@@ -13,5 +15,61 @@ public class App {
 		if (args.length > 0) {
 			System.out.println("Primeiro argumento: " + args[args.length - 1]);
 		}
+		
+		Date data = new Date();
+		System.out.println(data);
+		
+		System.out.println("Olá pessoal".charAt(2));
+		
+		String s = "Boa tarde";
+		System.out.println(s.concat("!!!"));
+		System.out.println(s + "!!!");
+		System.out.println(s.startsWith("Boa"));
+		System.out.println(s.toLowerCase().startsWith("boa"));
+		System.out.println(s.toUpperCase().endsWith("TARDE"));
+		System.out.println(s.length());
+		System.out.println(s.toLowerCase().equals("boa tarde"));
+		System.out.println(s.equalsIgnoreCase("boa tarde"));
+		
+		var nome = "Pedro";
+		var sobrenome = "Santos";
+		var idade = 33;
+		var salario = 12345.987;
+		
+		String maisUmaFrase = "Nome: " + nome + "\nSobrenome: "
+				+ sobrenome + "\nIdade: " + idade +
+				"\nSalario: " + salario + "\n\n";
+		System.out.println(maisUmaFrase);
+		
+		System.out.printf("O senhor %s %s tem %d anos e ganha R$%.2f.",
+				nome, sobrenome, idade, salario);
+		
+		String frase = String.format("\nO senhor %s %s tem %d anos e ganha R$%.2f.",
+				nome, sobrenome, idade, salario);
+		System.out.println(frase);
+		
+		System.out.println("Frase qualquer".contains("qual"));
+		System.out.println("Frase qualquer".indexOf("qual"));
+		System.out.println("Frase qualquer".substring(6));
+		System.out.println("Frase qualquer".substring(6, 10));
+		
+		String n1 = "7.9";
+		String n2 = "8.3";
+		
+		System.out.println(n1);
+		System.out.println(n2);
+		
+		BigDecimal numero1 = new BigDecimal(n1);
+		BigDecimal numero2 = new BigDecimal(n2);
+//		Double numero1 = Double.parseDouble(n1);
+//		Double numero2 = Double.parseDouble(n2);
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		
+		BigDecimal soma = numero1.add(numero2);
+//		Double soma = numero1 + numero2;
+		
+		System.out.println(soma);
 	}
 }
