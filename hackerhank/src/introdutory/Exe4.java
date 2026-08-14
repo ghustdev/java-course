bfa18f50603fbcfab520d1f12b1d42e5b457373f
ghustdev <cardosogustavo@discente.ufg.br>
2026-08-14 10:03:11 -0300

    feat: Add new exercises

--- hackerhank/src/introdutory/Exe4.java
+++ hackerhank/src/introdutory/Exe4.java
@@ -3,17 +3,31 @@ package introdutory;
 import java.util.Scanner;
 
 public class Exe4 {
+    private static final Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("================================");
-		for(int i=0;i<3;i++)
-		{
-			String s1=sc.next();
-			int x=sc.nextInt();
-			//Complete this line
-			System.out.printf("%-15s%03d%n", s1, x);
-		}
-		System.out.println("================================");
+		scanner.useLocale(java.util.Locale.US);
+
+		int T = 1;
+		// int T = scanner.nextInt();
+		// scanner.nextLine();
 		
+		while (T > 0) {
+			solve();
+			T--;
+		}
 	}
+
+
+	private static void solve() {
+		System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=scanner.next();
+            int x=scanner.nextInt();
+            
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
+	} 
 }
