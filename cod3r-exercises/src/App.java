40e7b1ee5877f7ffb7080a0ed32f4b80f91eb9e6
ghustdev <cardosogustavo@discente.ufg.br>
2026-08-14 14:34:16 -0300

    feat: configuration

--- cod3r-exercises/src/App.java
+++ cod3r-exercises/src/App.java
@@ -3,15 +3,15 @@ import initial.code.Artigo;
 import java.time.LocalDate;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        Artigo artigo1 = new Artigo("Java Basics", "John Doe", LocalDate.of(2023, 1, 15), "Programming");
-
-        System.out.println(artigo1);
-        artigo1.countHello();
-        if (args.length > 0) {
-            System.out.println("Primeiro argumento: " + args[args.length - 1]);
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		System.out.println("Hello, World!");
+		
+		Artigo artigo1 = new Artigo("Java Basics", "John Doe", LocalDate.of(2023, 1, 15), "Programming");
+		
+		System.out.println(artigo1);
+		artigo1.countHello();
+		if (args.length > 0) {
+			System.out.println("Primeiro argumento: " + args[args.length - 1]);
+		}
+	}
 }
